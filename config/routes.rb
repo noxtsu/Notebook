2446fe87390f2d763c600615f3d71fecd61f1273
noxtsu <kuraraiban29@gmail.com>
2022-07-06 05:55:56 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'todolists/new'
+  post 'todolists/' => 'todolists#create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+end
